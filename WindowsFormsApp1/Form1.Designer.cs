aab4d252dba1f4e4e61ff82fa0b3c9baf23d4e95
Mandurang <velichkomiroslav19@gmail.com>
2022-07-05 20:58:52 +0100

    added Save and Load func

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dgv = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +54,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(477, 377);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 2;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Load);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dgv);
             this.Name = "Form1";
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Load;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,12 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            dgv.DataSource = repo.RepoIntal();
+            //dgv.DataSource = repo.RepoIntal();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string fileName = "dfdfre";
             Excel.Application exApp = new Excel.Application();
 
             exApp.Workbooks.Add();
@@ -34,10 +36,76 @@ namespace WindowsFormsApp1
                 for (j = 0; j <= dgv.ColumnCount - 1; j++)
                 {
                     
+                    if (dgv[j, i].Value != null)
                     wsh.Cells[i + 1, j + 1] = dgv[j, i].Value.ToString();
                 }
             }
             exApp.Visible = true;
+            exApp.GetSaveAsFilename(fileName);
+
         }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            string[] columns = { "cibergod", "is", "good", "special", "Site" };
+
+            string[] data = { "abx", "asd", "qwe" ,"fdf"};
+
+            string fileName = "ExcelData";
+
+            LoadData(fileName, columns, data);
+        }
+
+        public void LoadData(string fileName ,string[] columns, string[] data)
+        {
+            addGridParam(columns, dgv);
+            addGridParam(data, dgv);
+
+            Excel.Application exApp = new Excel.Application();
+
+            exApp.Workbooks.Add();
+            Excel.Worksheet wsh = (Excel.Worksheet)exApp.ActiveSheet;
+
+            int i, j;
+            for (i = 0; i <= dgv.RowCount - 1; i++)
+            {
+                for (j = 0; j <= dgv.ColumnCount - 1; j++)
+                {
+
+                    if (dgv[j, i].Value != null)
+                        wsh.Cells[i + 1, j + 1] = dgv[j, i].Value.ToString();
+                }
+            }
+            exApp.Visible = true;
+            exApp.GetSaveAsFilename(fileName);
+
+        }
+
+        public void addGridParam(string[] N, DataGridView Grid)
+             
+        {
+
+            //пока столбцов не будет достаточное количество добавляем их
+
+            while (N.Length > Grid.ColumnCount)
+
+            {
+
+                //если колонок нехватает добавляем их пока их будет хватать
+
+                Grid.Columns.Add("", "");
+
+            }
+
+            //заполняем строку
+
+            Grid.Rows.Add(N);
+
+        }
+
+        //CreateExcelAsync(string fileName, string[] columns, string[] data)
+
+
+
     }
 }
